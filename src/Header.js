11f8ef5bfe8b482beda7029d4f976ec4a4a59b3e
Ivan <Zolotukhin.ivan.1@gmail.com>
2020-07-02 21:11:29 +0300

    Test push

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Profile</li>
+                <li>About</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
