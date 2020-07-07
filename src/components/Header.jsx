17ae8ffb27a2b30b2f7f65368c30df2ad838b032
Ivan <Zolotukhin.ivan.1@gmail.com>
2020-07-07 16:29:02 +0300

    Minor changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://www.thesmbguide.com/images/hatchful-by-shopify-1024x512-20190808.png' alt="logo"/>
+    </header>
+};
+
+export default Header;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,2 +1,24 @@
 import React from 'react';
 
+const Profile = () => {
+    return  <div className='content'>
+        <div className='yourImage'>
+            <img src="https://www.pulsar.ua/components/com_jshopping/files/img_products/full_71pOMS8Zv4L.jpg" alt="ProfileLogo"/>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>Post-1</div>
+                <div>Post-2</div>
+            </div>
+        </div>
+    </div>
+};
+
+export default Profile;
