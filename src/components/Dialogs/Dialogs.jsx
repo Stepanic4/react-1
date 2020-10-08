e8a789ca1c39f77277530e189a4e391b1d9ac5f0
Ivan <Zolotukhin.ivan.1@gmail.com>
2020-10-08 12:32:17 +0200

    change dialogs and minor design

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,31 @@ import React from 'react';
 import s from './Dialogs.module.css';
 
 const Dialogs = () => {
-  return (
-      <div>
-          <b className={s.item}>Dialogs</b>
-      </div>
-  )
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <b className={s.item}>Dialogs</b>
+                </div>
+                <div className={s.dialog}>
+                    <b className={s.item}>Your name</b>
+                </div>
+                <div className={s.dialog}>
+                    <b className={s.item}>John Fobos</b>
+                </div>
+                <div className={s.dialog}>
+                    <b className={s.item}>Czech Republic</b>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>More info</div>
+                <div className={s.message}>Cash 500</div>
+                <div className={s.message}>Don't stop</div>
+                <div className={s.message}>Programmer</div>
+            </div>
+        </div>
+    )
 };
 
 export default Dialogs;
