9bfad96555680e89a1b141ce6080c69389cf4694
Ivan <Zolotukhin.ivan.1@gmail.com>
2020-10-08 21:48:42 +0200

    Refractoring code Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,29 +2,41 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from 'react-router-dom';
 
+
+const DialogItem = (props) => {
+
+    let patch = '/dialogs/' + props.id;
+
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={patch}>{props.name}</NavLink>
+        </div>
+    )
+};
+
+const Message = (props) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+};
+
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
-            <div className={s.dialogsItem}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to='/dialogs/1'>Dialogs</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/2'>Your name</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/3'>John Fobos</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/4'>Czech Republic</NavLink>
-                </div>
+            <div className={s.dialogsItems}>
+                <DialogItem name='Ivan Zolotukhin' id='1'/>
+                <DialogItem name='John Fobos' id='2'/>
+                <DialogItem name='Your name' id='3'/>
+                <DialogItem name='Anna Mikhailovna' id='4'/>
+                <DialogItem name='Czech Republic' id='5'/>
             </div>
+
             <div className={s.messages}>
-                <div className={s.message}>Hello</div>
-                <div className={s.message}>More info</div>
-                <div className={s.message}>Cash 500</div>
-                <div className={s.message}>Don't stop</div>
-                <div className={s.message}>Programmer</div>
+                <Message message='Hello my friend' />
+                <Message message='More info' />
+                <Message message='Cash 500' />
+                <Message message="Don't stop" />
+                <Message message='Programmer is life and happy' />
             </div>
         </div>
     )
